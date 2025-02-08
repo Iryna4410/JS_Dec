944098d69bc965caff378ea8d35f093f0a54c1e8
Iryna4410 <Iryna.Kovbasnyuk@gmail.com>
2025-02-08 22:34:38 +0200

    HW1

---
+++ js_hw/main.js
@@ -0,0 +1,37 @@
+let k = 'hello';
+let h = 'owu';
+let g = 'com';
+let d = 'ua';
+let q = 1;
+let r = 10;
+let t = -999;
+let n = 123;
+let j = 3.14;
+let f = 2.7;
+let l = 16;
+let v = true;
+let z = false;
+console.log(h);
+console.log(g);
+console.log(k);
+console.log(d);
+console.log(q);
+console.log(r);
+console.log(t);
+console.log(n);
+console.log(j);
+console.log(f);
+console.log(l);
+console.log(v);
+console.log(z);
+
+let firstName = "Ковбаснюк";
+let middleName = "Ірина";
+let lastName = "Василівна"
+console.log(firstName +" "+ middleName +" "+ lastName);
+
+let a = 100; let b = '100'; let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
