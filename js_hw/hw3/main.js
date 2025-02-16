6f25eccd93032ea54c9b698584c241330d22712f
Iryna4410 <Iryna.Kovbasnyuk@gmail.com>
2025-02-16 22:57:04 +0200

    HW3

--- js_hw/hw3/main.js
+++ js_hw/hw3/main.js
@@ -4,25 +4,27 @@ for (i = 0; i < 10; i++) {
     document.write(`<div>Lorem Ipsum is</div>`);
 }
 
-
+document.write(`<h2>2</h2>`);
 // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
 for (i = 0; i < 10; i++) {
     document.write(`<div>Lorem ipsum is - ${i}</div>`);
 }
-
+document.write(`<h2>3</h2>`);
 // - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
 let x=0;
 while (x<20){
     document.write(`<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit!</h1>`);
     x++;
 }
+
+document.write(`<h2>4</h2>`);
 // -За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
 let a=0;
 while (a<20){
     document.write(`<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit!-${a}</h1>`);
     a++;
 }
-
+document.write(`<h2>5</h2>`);
 //
 // - Використовуючи данні з масиву, за допомоги document.write та циклу
 // побудувати структуру по шаблону
@@ -48,7 +50,7 @@ for (const item of listOfItems){
 
 }
 document.write(`</ul>`)
-
+document.write(`<h2>6</h2>`);
 // Використовуючи данні з масиву, за допомоги document.write та циклу
 // побудувати структуру по шаблону
 // Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон
@@ -80,6 +82,48 @@ let products = [
     {
         title: 'tea',
         price: 15,
-        image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+        image: 'https://akva-svit.com.ua/image/cache/catalog/articles/blog-novosti/329-1200x650.webp'
     },
 ];
+
+for (product of products){
+    document.write(`<div class="product-card">
+     <h3 class="product-title">${product.title}. Price -${product.price}</h3>
+ <img src="${product.image}" alt="" class="product-image">
+ </div>`)
+}
+
+
+// є масив
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+// за допомоги циклу вивести:
+//     - користувачів зі статусом true
+// - користувачів зі статусом false
+// - користувачів які старші за 30 років
+for (const user of users) {
+    if (user.status===true) {
+        console.log(user);
+    }
+
+}
+for (const user of users){
+if(user.status===false){
+    console.log(user);
+
+}}
+for (const user of users){
+    if (user.age>30){}
+    {console.log(user);}
+}
